cdb101107aa56e09f6fc157c7eab6415b56062f9
jin <jin337x@outlook.com>
2025-04-14 10:35:46 +0800

    feat: 设置基座

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,8 @@ import legacy from '@vitejs/plugin-legacy'
 import { resolve } from 'path'
 import compression from 'vite-plugin-compression'
 
+import federation from '@originjs/vite-plugin-federation'
+
 export default defineConfig({
   server: {
     open: true,
@@ -23,6 +25,62 @@ export default defineConfig({
     compression({ threshold: 10240 }),
     legacy({
       targets: ['defaults', 'not IE 11'],
+    }),
+    federation({
+      name: 'hostApp',
+      filename: 'remoteEntry.js',
+      remotes: {
+        // 工程管理
+        projectApp: {
+          external: 'http://192.168.0.42:3001/project/remoteEntry.js',
+          from: 'vite',
+          format: 'esm',
+        },
+        // 财务管理
+        financeApp: {
+          external: 'http://192.168.0.42:3002/finance/remoteEntry.js',
+          from: 'vite',
+          format: 'esm',
+        },
+        // 审批
+        oaApp: {
+          external: 'http://192.168.0.42:3003/oa/remoteEntry.js',
+          from: 'vite',
+          format: 'esm',
+        },
+        // 人力资源
+        hrmApp: {
+          external: 'http://192.168.0.42:3004/hrm/remoteEntry.js',
+          from: 'vite',
+          format: 'esm',
+        },
+      },
+      shared: {
+        react: {
+          singleton: true,
+          requiredVersion: '^18.3.1',
+        },
+        'react-dom': {
+          singleton: true,
+          requiredVersion: '^18.3.1',
+        },
+        'react-router': {
+          singleton: true,
+          requiredVersion: '^7.4.1',
+        },
+        'react-redux': {
+          singleton: true,
+          requiredVersion: '^9.2.0',
+        },
+        '@reduxjs/toolkit': {
+          singleton: true,
+          requiredVersion: '^2.6.1',
+        },
+        axios: {
+          singleton: true,
+          requiredVersion: '^1.8.4',
+        },
+      },
     }),
   ],
   build: {
