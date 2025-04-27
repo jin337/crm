1d2411bd2256790b5cdbcedd19bc6579c19dd630
jin <jin337x@outlook.com>
2025-04-27 14:35:03 +0800

    ci: 新增打包设置

--- vite.config.js
+++ vite.config.js
@@ -41,5 +41,16 @@ export default defineConfig({
     target: 'esnext',
     minify: false,
     cssCodeSplit: false,
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/js/[name]-[hash].js',
+        chunkFileNames: 'assets/js/chunks/[name]-[hash].js',
+        assetFileNames: 'assets/[ext]/[name]-[hash].[ext]',
+        manualChunks: {
+          'chunk-vendor': ['react', 'react-dom', 'react-router', 'react-redux', '@reduxjs/toolkit', 'axios'],
+          'chunk-other': ['@arco-design/web-react'],
+        },
+      },
+    },
   },
 })
